533af3dab3b600948362f6413f30414da5fff274
Hyuni Kim <spaceship00@naver.com>
2024-01-18 12:23:11 +0000

    google.com signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider, User, onAuthStateChanged } from 'firebase/auth';
+import googleSignIn from './google_signin.png';
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user != null) {
+        if (!user.email?.endsWith('google.com')) {
+          alert('Only google.com emails are available.');
+          signOut(auth);
+          return;
+        }
+      }
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  async function signIn() {
+    const auth = getAuth();
+    if (auth.currentUser != null) {
+      return;
+    }
+
+    const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({
+      prompt: 'select_account'
+    });
+    const result = await signInWithPopup(auth, provider);
+    setUser(result.user);
+  }
+
   return (
     <div className={styles.App}>
       <div className={styles.Title}>GMASH</div>
-      <button className={styles.Button}>Start session</button>
+      {!user && <img className={styles.GoogleSignIn} src={googleSignIn} onClick={signIn} />}
+      {user && <button className={styles.Button}>Start session</button>}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaVS0MghioqcmFTBdwNrdV9P5Zpu2ilUs",
+  authDomain: "gmash-496a9.firebaseapp.com",
+  projectId: "gmash-496a9",
+  storageBucket: "gmash-496a9.appspot.com",
+  messagingSenderId: "131659335029",
+  appId: "1:131659335029:web:d675ce7e9cbe1dba753f3c",
+  measurementId: "G-X17ZEEE5DZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
