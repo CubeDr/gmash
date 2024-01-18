f59c880f3782fee7a6b24b1daeca0d0a89d1ae4f
Hyuni Kim <spaceship00@naver.com>
2024-01-18 11:31:42 +0000

    Build initial page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <div className={styles.Title}>GMASH</div>
+      <button className={styles.Button}>Start session</button>
+    </div>
+  );
+}
+
+export default App;
