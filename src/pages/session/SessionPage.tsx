2ef7b756c4c5a9b9f33ffb9dd20769beb1c17280
Hyuni Kim <spaceship00@naver.com>
2024-01-19 03:40:39 +0000

    Divide session page sections

--- src/pages/session/SessionPage.tsx
+++ src/pages/session/SessionPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import useSelectedMembers from './useSelectedMembers';
 import Court from './court/Court';
+import styles from './SessionPage.module.css';
 
 export default function SessionPage() {
     const { members } = useSelectedMembers();
@@ -11,7 +12,11 @@ export default function SessionPage() {
 
     return (
         <div>
+            <h4 className={styles.SectionTitle}>Now playing</h4>
             <Court team1={members.slice(0, 2)} team2={members.slice(2, 4)} />
+            <h4 className={styles.SectionTitle}>Upcoming</h4>
+            <Court team1={members.slice(0, 2)} team2={members.slice(2, 4)} />
+            <h4 className={styles.SectionTitle}>Members</h4>
         </div>
     );
 }
