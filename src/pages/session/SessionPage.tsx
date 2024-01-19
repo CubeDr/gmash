cf533ddbe6f3d89027f62b0ae9df4244772e3721
Hyuni Kim <spaceship00@naver.com>
2024-01-19 04:12:14 +0000

    Show game button on member select

--- src/pages/session/SessionPage.tsx
+++ src/pages/session/SessionPage.tsx
@@ -1,12 +1,27 @@
 import useSessionMembers from './useSessionMembers';
 import Court from './court/Court';
 import styles from './SessionPage.module.css';
+import { useRef, useState } from 'react';
+import { Member } from '../../data/member';
 
 export default function SessionPage() {
     const { members } = useSessionMembers();
+    const [selectedMemberIdsCount, setSelectedMemberIdsCount] = useState(0);
+    const selectedMemberIdsRef = useRef(new Set<String>());
+
+    function onMemberClick(element: HTMLElement, member: Member) {
+        if (element.classList.contains(styles.SelectedMember)) {
+            element.classList.remove(styles.SelectedMember);
+            selectedMemberIdsRef.current.delete(member.id);
+        } else if (selectedMemberIdsRef.current.size < 4) {
+            element.classList.add(styles.SelectedMember);
+            selectedMemberIdsRef.current.add(member.id);
+        }
+        setSelectedMemberIdsCount(selectedMemberIdsRef.current.size);
+    }
 
     return (
-        <div>
+        <div className={selectedMemberIdsCount > 0 ? styles.PaddingBottom : ''}>
             <h4 className={styles.SectionTitle}>Now playing</h4>
             <Court team1={members.slice(0, 2)} team2={members.slice(2, 4)} />
             <h4 className={styles.SectionTitle}>Upcoming</h4>
@@ -15,7 +30,7 @@ export default function SessionPage() {
             <div className={styles.Members}>
                 {
                     members.map(member => (
-                        <div className={styles.Member}>
+                        <div className={styles.Member} onClick={e => onMemberClick(e.target as HTMLElement, member)}>
                             <div className={styles.MemberRow}>
                                 <span>{member.name}</span>
                                 <span>{member.elo}</span>
@@ -25,6 +40,11 @@ export default function SessionPage() {
                     ))
                 }
             </div>
+            {selectedMemberIdsCount > 0 &&
+                <button className={styles.Button}>
+                    Make a game ({selectedMemberIdsCount})
+                </button>
+            }
         </div>
     );
 }
