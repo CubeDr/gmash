22f3968d93aac86358a9a72842357f1d383d77a7
Hyuni Kim <spaceship00@naver.com>
2024-01-19 03:51:32 +0000

    Show members

--- src/pages/session/SessionPage.tsx
+++ src/pages/session/SessionPage.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from 'react';
 import useSelectedMembers from './useSelectedMembers';
 import Court from './court/Court';
 import styles from './SessionPage.module.css';
 
 export default function SessionPage() {
     const { members } = useSelectedMembers();
-
-    useEffect(() => {
-        console.log(members);
-    }, [members]);
 
     return (
         <div>
@@ -17,6 +12,19 @@ export default function SessionPage() {
             <h4 className={styles.SectionTitle}>Upcoming</h4>
             <Court team1={members.slice(0, 2)} team2={members.slice(2, 4)} />
             <h4 className={styles.SectionTitle}>Members</h4>
+            <div className={styles.Members}>
+                {
+                    members.map(member => (
+                        <div className={styles.Member}>
+                            <div className={styles.MemberRow}>
+                                <span>{member.name}</span>
+                                <span>{member.elo}</span>
+                            </div>
+                            <span className={styles.GamesToday}>0 games played today</span>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
